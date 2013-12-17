2c2a23176b502dccb69a73ac031f7c55f39cd50d
Naomi Aro <naomiaro@gmail.com>
2013-12-17 13:05:39 -0500

    CC-5645 : Replace PHP APC

--- airtime_mvc/application/models/Cache.php
+++ airtime_mvc/application/models/Cache.php
@@ -2,6 +2,7 @@
 
 class Cache
 {
+
 	private function createCacheKey($key, $isUserValue, $userId = null) {
 
 		$CC_CONFIG = Config::getConfig();
@@ -18,32 +19,38 @@ class Cache
 	}
 
 	private static function getMemcached() {
-
-	    $CC_CONFIG = Config::getConfig();
-
 	    $memcached = new Memcached();
-	    //$server is in the format "host:port"
-	    foreach($CC_CONFIG['memcached']['servers'] as $server) {
-
-	        list($host, $port) = explode(":", $server);
-	        $memcached->addServer($host, $port);
-	    }
+	    $memcached->addServer('localhost', 11211);
 
 	    return $memcached;
 	}
 
 	public function store($key, $value, $isUserValue, $userId = null) {
 
-		//$cacheKey = self::createCacheKey($key, $userId);
-		return false; ///apc_store($cacheKey, $value);
+        $cache = self::getMemcached();
+		$cacheKey = self::createCacheKey($key, $userId);
+
+		return $cache->set($cacheKey, $value);
 	}
 
 	public function fetch($key, $isUserValue, $userId = null) {
 
-		//$cacheKey = self::createCacheKey($key, $isUserValue, $userId);
-		return false; //apc_fetch($cacheKey);
+	    $cache = self::getMemcached();
+		$cacheKey = self::createCacheKey($key, $isUserValue, $userId);
+		$value = $cache->get($cacheKey);
+
+		$found = true;
+		if ($cache->getResultCode() == Memcached::RES_NOTFOUND) {
+            $found = false;
+		}
+
+		//need to return something to distinguish a cache miss from a stored "false" preference.
+		return array(
+		   "found" => $found,
+		   "value" => $value,
+		);
 	}
-	
+
     public static function clear() {
         // Disabled on SaaS
         // apc_clear_cache('user');
