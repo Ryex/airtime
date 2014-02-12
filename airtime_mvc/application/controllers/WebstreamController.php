17f02a3cc5b8efc62e3c2ed439203ce0b9bb4ad2
Naomi <naomiaro@gmail.com>
2014-02-12 14:59:11 -0500

    CC-5450 : Refactor Media Management (Classes/DB) in Airtime
    working on getting deleting items to be the same as in 2.5.1

--- airtime_mvc/application/controllers/WebstreamController.php
+++ airtime_mvc/application/controllers/WebstreamController.php
@@ -19,16 +19,16 @@ class WebstreamController extends Zend_Controller_Action
     {
     	//clear the session in case an old playlist was open: CC-4196
     	Application_Model_Library::changePlaylist(null, null);
-    	
+
     	$service = new Application_Service_WebstreamService();
     	$form = $service->makeWebstreamForm(null);
-    	
+
     	$form->setDefaults(array(
     		'name' => 'Unititled Webstream',
     		'hours' => 0,
     		'mins' => 30,
     	));
-    	
+
     	$this->view->action = "new";
     	$this->view->html = $form->render();
     }
@@ -37,7 +37,7 @@ class WebstreamController extends Zend_Controller_Action
     {
         $request = $this->getRequest();
         $id = $request->getParam("id");
-        
+
         $service = new Application_Service_WebstreamService();
     	$form = $service->makeWebstreamForm($id, true);
 
@@ -52,45 +52,39 @@ class WebstreamController extends Zend_Controller_Action
         $request = $this->getRequest();
         $ids = $request->getParam("ids");
 
-        Application_Model_Library::changePlaylist(null, null);
-        
-        $service = new Application_Service_WebstreamService();
-        $service->deleteWebstreams($ids);
-
-        $this->view->action = "delete";
-        $this->view->html = $this->view->render('form/webstream.phtml');
-
+        $service = new Application_Service_MediaService();
+        $service->delete($ids);
     }
 
     public function saveAction()
     {
         $request = $this->getRequest();
         $parameters = array();
-        
+
         foreach (array('id','hours', 'mins', 'name','description','url') as $p) {
             $parameters[$p] = trim($request->getParam($p));
         }
-        
+
         Logging::info($parameters);
-        
+
         $service = new Application_Service_WebstreamService();
         $form = $service->makeWebstreamForm(null);
-        
+
         if ($form->isValid($parameters)) {
         	Logging::info("form is valid");
-        	
+
         	$values = $form->getValues();
         	Logging::info($values);
-        	
+
         	$ws = $service->saveWebstream($values);
-        	
+
         	$this->view->statusMessage = "<div class='success'>"._("Webstream saved.")."</div>";
         	$this->view->streamId = $ws->getId();
         	$this->view->length = "00:05"; //$di->format("%Hh %Im");
         }
         else {
         	Logging::info("form is not valid");
-        	
+
         	$this->view->statusMessage = "<div class='errors'>"._("Invalid form values.")."</div>";
         	$this->view->streamId = -1;
         	$this->view->errors = $form->getMessages();
