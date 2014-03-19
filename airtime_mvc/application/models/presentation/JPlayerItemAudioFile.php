04220984dd902aa16f5b54c38461dac207621272
Naomi <naomiaro@gmail.com>
2014-03-19 19:27:49 -0400

    CC-5450 : Refactor Media Management (Classes/DB) in Airtime
    working jPlayer, enabling remove controls, finishing playlist preview request.

--- airtime_mvc/application/models/presentation/JPlayerItemAudioFile.php
+++ airtime_mvc/application/models/presentation/JPlayerItemAudioFile.php
@@ -2,28 +2,20 @@
 
 class Presentation_JPlayerItemAudioFile extends Presentation_JPlayerItem
 {
-	public function hasMultiple() {
-		return false;
-	}
-	
-	public function hasDuration() {
-		return true;
-	}
-	
 	protected function compute() {
-		
-		$mime = parent::convertMime();
-		
+
+		$mime = parent::convertMime($this->media->getMime());
+
 		if (is_null($mime)) {
 			return array();
 		}
-		
+
 		$item =  array(
 			"title" => $this->media->getName(),
 			"artist" => $this->media->getCreator(),
 			$mime => $this->media->getURI()
 		);
-			
+
 		return array($item);
 	}
 }
