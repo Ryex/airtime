50b19510786b0b530bed95e70bd006b08bc61bef
Naomi <naomiaro@gmail.com>
2014-03-25 14:11:10 -0400

    CC-5450 : Refactor Media Management (Classes/DB) in Airtime
    finishing up last relative date rules.

--- airtime_mvc/application/models/airtime/Playlist.php
+++ airtime_mvc/application/models/airtime/Playlist.php
@@ -10,6 +10,9 @@ use \PropelPDO;
 use \Exception;
 use \Logging;
 use \Propel;
+use \DateTime;
+use \DateTimeZone;
+use \DateInterval;
 
 
 /**
@@ -36,13 +39,94 @@ abstract class Playlist extends BasePlaylist implements \Interface_Playlistable
 		return $this->getClassKey() === intval(PlaylistPeer::CLASSKEY_0);
 	}
 	
+	/*
+	0 => _("Select modifier"),
+	1 => _("contains"),
+	2 => _("does not contain"),
+	3 => _("is"),
+	4 => _("is not"),
+	5 => _("starts with"),
+	6 => _("ends with")
+	7 => _("is greater than"),
+	8 => _("is less than"),
+	9 => _("is greater than or equal to"),
+	10 => _("is less than or equal to"),
+	11 => _("is in the range")
+	12 => _("today"),
+    13 => _("yesterday"),
+	14 => _("this week"),
+	15 => _("last week"),
+	16 => _("this month"),
+	17 => _("last month"),
+	18 => _("this year"),
+	19 => _("last year"),
+	20 => _("in the last"),
+	21 => _("not in the last")
+	 */
+	
 	protected function getCriteriaRules(&$query) {
+		
+		$displayTimezone = new DateTimeZone(\Application_Model_Preference::GetUserTimezone());
+		$utcTimezone = new DateTimeZone("UTC");
+		$now = new DateTime("now", $displayTimezone);
+		
+		/*
+		 * @param DateTime $now
+		*/
+		$getWeekStartDateTime = function ($now) {
+			//sunday = 0
+			$weekStartDay = intval(\Application_Model_Preference::GetWeekStartDay());
+		
+			$dofw = intval($now->format("w"));
+				
+			$daysSub = $dofw - $weekStartDay;
+			if ($daysSub < 0) {
+				$daysSub += 7;
+			}
+				
+			$interval = new DateInterval("P{$daysSub}D");
+				
+			$weekStart = clone $now;
+			$weekStart->setTime(0, 0, 0);
+			$weekStart->sub($interval);
+		
+			return $weekStart;
+		};
+		
+		/*
+		 * @param DateTime $now
+		*/
+		$getMonthStartDateTime = function ($now) {
+			$monthStart = clone $now;
+			
+			$year = intval($monthStart->format("Y"));
+			$month = intval($monthStart->format("m"));
+			$monthStart->setDate($year, $month, 1);
+			$monthStart->setTime(0, 0, 0);
+		
+			return $monthStart;
+		};
+		
+		/*
+		 * @param DateTime $now
+		*/
+		$getYearStartDateTime = function ($now) {
+			$yearStart = clone $now;
+				
+			$year = intval($yearStart->format("Y"));
+			$yearStart->setDate($year, 1, 1);
+			$yearStart->setTime(0, 0, 0);
+		
+			return $yearStart;
+		};
 		
 		//$pattern is like "%VALUE%", or just "VALUE" if % is not needed.
 		function createRule(&$query, $comparison, $pattern = "VALUE") {
-			return function($col, $value1, $value2 = null) use (&$query, $comparison, $pattern) {
+			return function($col, $data) use (&$query, $comparison, $pattern) {
 				
 				$m = $query->getModelName();
+				
+				$value1 = $data["input1"];
 				
 				$name = mt_rand(10000, 99999);
 				$cond = "{$m}.{$col} {$comparison} ?";
@@ -53,11 +137,25 @@ abstract class Playlist extends BasePlaylist implements \Interface_Playlistable
 			};
 		}
 		
-		$range = function ($col, $value1, $value2) use (&$query) {
+		$contains = createRule($query, Criteria::ILIKE, "%VALUE%");
+		$doesntContain = createRule($query, Criteria::NOT_ILIKE, "%VALUE%");
+		$is = createRule($query, Criteria::EQUAL);
+		$isNot = createRule($query, Criteria::NOT_EQUAL);
+		$startsWith = createRule($query, Criteria::ILIKE, "VALUE%");
+		$endsWith = createRule($query, Criteria::ILIKE, "%VALUE");
+		$isGreaterThan = createRule($query, Criteria::GREATER_THAN);
+		$isLessThan = createRule($query, Criteria::LESS_THAN);
+		$isGreaterThanEqualTo = createRule($query, Criteria::GREATER_EQUAL);
+		$isLessThanEqualTo = createRule($query, Criteria::LESS_EQUAL);
+		
+		$range = function ($col, $data) use (&$query) {
 			
 			$name1 = mt_rand(10000, 99999);
 			$name2 = mt_rand(10000, 99999);
 			$name3 = mt_rand(10000, 99999);
+			
+			$value1 = $data["input1"];
+			$value2 = $data["input2"];
 			
 			$m = $query->getModelName();
 			
@@ -75,16 +173,272 @@ abstract class Playlist extends BasePlaylist implements \Interface_Playlistable
 			return $name3;
 		};
 		
-		$contains = createRule($query, Criteria::ILIKE, "%VALUE%");
-		$doesntContain = createRule($query, Criteria::NOT_ILIKE, "%VALUE%");
-		$is = createRule($query, Criteria::EQUAL);
-		$isNot = createRule($query, Criteria::NOT_EQUAL);
-		$startsWith = createRule($query, Criteria::ILIKE, "VALUE%");
-		$endsWith = createRule($query, Criteria::ILIKE, "%VALUE");
-		$isGreaterThan = createRule($query, Criteria::GREATER_THAN);
-		$isLessThan = createRule($query, Criteria::LESS_THAN);
-		$isGreaterThanEqualTo = createRule($query, Criteria::GREATER_EQUAL);
-		$isLessThanEqualTo = createRule($query, Criteria::LESS_EQUAL);
+		$today = function($col) use (&$query, $utcTimezone, $now, $range) {
+			
+			$now->setTime(0, 0, 0);
+			$interval = new DateInterval("P1D");
+			
+			$tomorrow = clone $now;
+			$tomorrow->add($interval);
+			
+			$now->setTimezone($utcTimezone);
+			$tomorrow->setTimezone($utcTimezone);
+			
+			$from = $now->format('Y-m-d H:i:s');
+			$to = $tomorrow->format('Y-m-d H:i:s');
+			
+			$data = array(
+				"input1" => $from,
+				"input2" => $to		
+			);
+			
+			return $range($col, $data);
+		};
+		
+		$yesterday = function($col) use (&$query, $utcTimezone, $now, $range) {
+			
+			$now->setTime(0, 0, 0);	
+			$interval = new DateInterval("P1D");
+				
+			$yesterday = clone $now;
+			$yesterday->sub($interval);
+				
+			$now->setTimezone($utcTimezone);
+			$yesterday->setTimezone($utcTimezone);
+				
+			$from = $yesterday->format('Y-m-d H:i:s');
+			$to = $now->format('Y-m-d H:i:s');
+				
+			$data = array(
+				"input1" => $from,
+				"input2" => $to		
+			);
+			
+			return $range($col, $data);
+		};
+		
+		$thisWeek = function($col) use (&$query, $utcTimezone, $now, $range, $getWeekStartDateTime) {
+			
+			$weekStart = $getWeekStartDateTime($now);
+			
+			$now->setTimezone($utcTimezone);
+			$weekStart->setTimezone($utcTimezone);
+			
+			$from = $weekStart->format('Y-m-d H:i:s');
+			$to = $now->format('Y-m-d H:i:s');
+			
+			$data = array(
+				"input1" => $from,
+				"input2" => $to		
+			);
+			
+			return $range($col, $data);
+		};
+		
+		$lastWeek = function($col) use (&$query, $utcTimezone, $now, $range, $getWeekStartDateTime) {
+			
+			$weekStart = $getWeekStartDateTime($now);
+			$weekEnd = clone $weekStart;
+			
+			$interval = new DateInterval("P7D");
+			$weekStart->sub($interval);
+			
+			$weekStart->setTimezone($utcTimezone);
+			$weekEnd->setTimezone($utcTimezone);
+			
+			$from = $weekStart->format('Y-m-d H:i:s');
+			$to = $weekEnd->format('Y-m-d H:i:s');
+			
+			$data = array(
+				"input1" => $from,
+				"input2" => $to		
+			);
+			
+			return $range($col, $data);
+		};
+		
+		$thisMonth = function($col) use (&$query, $utcTimezone, $now, $range, $getMonthStartDateTime) {
+		
+			$monthStart = $getMonthStartDateTime($now);
+				
+			$monthStart->setTimezone($utcTimezone);
+			$now->setTimezone($utcTimezone);
+				
+			$from = $monthStart->format('Y-m-d H:i:s');
+			$to = $now->format('Y-m-d H:i:s');
+				
+			$data = array(
+				"input1" => $from,
+				"input2" => $to		
+			);
+			
+			return $range($col, $data);
+		};
+		
+		$lastMonth = function($col) use (&$query, $utcTimezone, $now, $range, $getMonthStartDateTime) {
+		
+			$monthStart = $getMonthStartDateTime($now);
+			$monthEnd = clone $monthStart;
+			
+			$interval = new DateInterval("P1M");
+			$monthStart->sub($interval);
+		
+			$monthStart->setTimezone($utcTimezone);
+			$monthEnd->setTimezone($utcTimezone);
+		
+			$from = $monthStart->format('Y-m-d H:i:s');
+			$to = $monthEnd->format('Y-m-d H:i:s');
+		
+			$data = array(
+				"input1" => $from,
+				"input2" => $to		
+			);
+			
+			return $range($col, $data);
+		};
+		
+		$thisYear = function($col) use (&$query, $utcTimezone, $now, $range, $getYearStartDateTime) {
+		
+			$yearStart = $getYearStartDateTime($now);
+			
+			$yearStart->setTimezone($utcTimezone);
+			$now->setTimezone($utcTimezone);
+		
+			$from = $yearStart->format('Y-m-d H:i:s');
+			$to = $now->format('Y-m-d H:i:s');
+		
+			$data = array(
+				"input1" => $from,
+				"input2" => $to		
+			);
+			
+			return $range($col, $data);
+		};
+		
+		$lastYear = function($col) use (&$query, $utcTimezone, $now, $range, $getYearStartDateTime) {
+		
+			$yearStart = $getYearStartDateTime($now);
+			$yearEnd = clone $yearStart;
+			
+			$interval = new DateInterval("P1Y");
+			$yearStart->sub($interval);
+				
+			$yearStart->setTimezone($utcTimezone);
+			$yearEnd->setTimezone($utcTimezone);
+		
+			$from = $yearStart->format('Y-m-d H:i:s');
+			$to = $yearEnd->format('Y-m-d H:i:s');
+		
+			$data = array(
+				"input1" => $from,
+				"input2" => $to		
+			);
+			
+			return $range($col, $data);
+		};
+		
+		/*
+		 	1 => _("seconds"),
+			2 => _("minutes"),
+			3 => _("hours"),
+			4 => _("days"),
+			5 => _("weeks"),
+			6 => _("months"),
+			7 => _("years"),
+		 */
+		$convertRelativeUnit = function($duration, $unit) {
+			
+			$timespan = intval($duration);
+			
+			switch($unit) {
+				
+				case 1:
+					$format = "PT{$timespan}S";
+					break;
+				case 2:
+					$format = "PT{$timespan}M";
+					break;
+				case 3:
+					$format = "PT{$timespan}H";
+					break;
+				case 4:
+					$format = "P{$timespan}D";
+					break;
+				case 5:
+					$timespan *= 7;
+					$format = "P{$timespan}D";
+					break;
+				case 6:
+					$format = "P{$timespan}M";
+					break;
+				case 7:
+					$format = "P{$timespan}Y";
+					break;
+			}
+			
+			return new DateInterval($format);
+		};
+		
+		$getRelativeDateTimes = function($now, $data, $utcTimezone, $convertRelativeUnit) {
+			
+			$value1 = $data["input1"];
+			$unit1 = $data["unit1"];
+			$interval1 = $convertRelativeUnit($value1, $unit1);
+				
+			$from = clone $now;
+			$to = clone $now;
+			
+			//relative range rule.
+			if (isset($data["input2"]) && isset($data["unit2"])) {
+				$value2 = $data["input2"];
+				$unit2 = $data["unit2"];
+				$interval2 = $convertRelativeUnit($value2, $unit2);
+			
+				$from->sub($interval2);
+				$to->sub($interval1);
+			}
+			else {
+				$from->sub($interval1);
+			}
+				
+			$from->setTimezone($utcTimezone);
+			$to->setTimezone($utcTimezone);
+			
+			return array($from, $to);
+		};
+
+		$inTheLast = function($col, $data) use (&$query, $utcTimezone, $now, $getRelativeDateTimes, $convertRelativeUnit, $range) {
+		
+			list($from, $to) = $getRelativeDateTimes($now, $data, $utcTimezone, $convertRelativeUnit);
+
+			$data = array(
+				"input1" => $from->format('Y-m-d H:i:s'),
+				"input2" => $to->format('Y-m-d H:i:s')		
+			);
+			
+			return $range($col, $data);
+		};
+		
+		$notInTheLast = function($col, $data) use (&$query, $utcTimezone, $now, $getRelativeDateTimes, $convertRelativeUnit, $isLessThan, $isGreaterThan) {
+		
+			list($from, $to) = $getRelativeDateTimes($now, $data, $utcTimezone, $convertRelativeUnit);
+
+			$data1 = array(
+				"input1" => $from->format('Y-m-d H:i:s')
+			);
+			
+			$data2 = array(
+				"input1" => $to->format('Y-m-d H:i:s')
+			);
+			
+			$name1 = $isLessThan($col, $data1);
+			$name2 = $isGreaterThan($col, $data2);
+			$name3 = mt_rand(10000, 99999);
+			
+			$query->combine(array($name1, $name2), 'or', $name3);
+			
+			return $name3;
+		};
 		
 		return array(
 			null,
@@ -98,7 +452,17 @@ abstract class Playlist extends BasePlaylist implements \Interface_Playlistable
 			$isLessThan,
 			$isGreaterThanEqualTo,
 			$isLessThanEqualTo,
-			$range
+			$range,
+			$today,
+			$yesterday,
+			$thisWeek,
+			$lastWeek,
+			$thisMonth,
+			$lastMonth,
+			$thisYear,
+			$lastYear,
+			$inTheLast,
+			$notInTheLast
 		);
 	}
 	
@@ -204,10 +568,7 @@ abstract class Playlist extends BasePlaylist implements \Interface_Playlistable
     				$rule = $criteriaRules[$orBlock["modifier"]];
     
     				$column = $orBlock["criteria"];
-    				$input1 = $orBlock["input1"];
-    				$input2 = isset($orBlock["input2"]) ? $orBlock["input2"] : null;
-    
-    				$condition = $rule($column, $input1, $input2);
+    				$condition = $rule($column, $orBlock);
     
     				$conditionOr[] = $condition;
     			}
