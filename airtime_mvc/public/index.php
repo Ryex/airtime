fe403415f76240b1533c74c728c469c3b216febd
Albert Santoni <albert.santoni@sourcefabric.org>
2015-02-02 18:03:13 -0500

    Log all PHP errors (including parse errors) to our zend log

--- airtime_mvc/public/index.php
+++ airtime_mvc/public/index.php
@@ -52,6 +52,7 @@ $application = new Zend_Application(
 
 require_once (APPLICATION_PATH."/logging/Logging.php");
 Logging::setLogPath('/var/log/airtime/zendphp.log');
+Logging::setupParseErrorLogging();
 
 // Create application, bootstrap, and run
 try {
